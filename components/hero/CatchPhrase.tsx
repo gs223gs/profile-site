30286863f2e7628bff5657aae10c61c68a4a026d
T.Miura <mtaiga0213@icloud.com>
2025-07-14 21:00:20 +0900

    feat: キャッチコピーコンポーネントの実装

---
+++ components/hero/CatchPhrase.tsx
@@ -0,0 +1,9 @@
+export const CatchPhrase = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center text-center">
+      <h1 className="text-3xl font-bold text-gray-900 mb-4">
+        志は身長より高く
+      </h1>
+    </div>
+  );
+};
