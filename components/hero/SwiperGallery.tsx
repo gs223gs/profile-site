2965b3ab36693c5baeb6114ce9d397614bf1fc68
T.Miura <mtaiga0213@icloud.com>
2025-07-14 21:09:10 +0900

    feat: SwiperGalleryコンポーネントを改良し、画像データ型を追加

--- components/hero/SwiperGallery.tsx
+++ components/hero/SwiperGallery.tsx
@@ -1,22 +1,48 @@
 
-'use client';
+'use client'
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css'; // グローバル import
-import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react'
+import Image from 'next/image'
+import 'swiper/css'
+import { Autoplay } from 'swiper/modules'
+import { ImageData } from '@/types/image'
 
-export const SwiperGallery = ({ images }: { images: string[] }) => (
-  <Swiper
-    modules={[Autoplay]}
-    autoplay={{ delay: 3000 }}
-    loop
-    className="w-full h-64"
-  >
-    {images.map(src => (
-      <SwiperSlide key={src}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={src} alt="" className="object-cover w-full h-full" />
-      </SwiperSlide>
-    ))}
-  </Swiper>
-);
+interface SwiperGalleryProps {
+  images: ImageData[]
+}
+
+export const SwiperGallery = ({ images }: SwiperGalleryProps) => {
+  console.log('SwiperGallery images:', images) // デバッグ用
+
+  return (
+    <div className="w-full h-full">
+      <Swiper
+        modules={[Autoplay]}
+        autoplay={{ delay: 3000 }}
+        loop
+        className="w-full h-full"
+      >
+        {images.map((image) => (
+          <SwiperSlide key={image.id}>
+            <div className="relative w-full h-full min-h-[200px]">
+              <Image
+                src={image.src}
+                alt={image.alt}
+                fill
+                className="object-cover"
+                sizes="(max-width: 768px) 100vw, 50vw"
+                priority={image.id === 1}
+                onError={(e) => {
+                  console.error('画像読み込みエラー:', image.src, e)
+                }}
+                onLoad={() => {
+                  console.log('画像読み込み成功:', image.src)
+                }}
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  )
+}
