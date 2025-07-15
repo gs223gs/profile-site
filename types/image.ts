45ae26a634a9ec891c792d15f92d4b285ef05f69
T.Miura <mtaiga0213@icloud.com>
2025-07-15 12:03:17 +0900

    feat: 画像データを型付け

---
+++ types/image.ts
@@ -0,0 +1,38 @@
+export interface ImageData {
+  id: number
+  src: string
+  alt: string
+  width?: number
+  height?: number
+}
+
+export const profileImages: ImageData[] = [
+  {
+    id: 1,
+    src: '/T-Miura1.jpg',
+    alt: 'T.Miura プロフィール写真 1',
+    width: 400,
+    height: 400
+  },
+  {
+    id: 2,
+    src: '/T-Miura2.jpg',
+    alt: 'T.Miura プロフィール写真 2',
+    width: 400,
+    height: 400
+  },
+  {
+    id: 3,
+    src: '/T-Miura3.png',
+    alt: 'T.Miura プロフィール写真 3',
+    width: 400,
+    height: 400
+  },
+  {
+    id: 4,
+    src: '/T-Miura4.PNG',
+    alt: 'T.Miura プロフィール写真 4',
+    width: 400,
+    height: 400
+  }
+]
