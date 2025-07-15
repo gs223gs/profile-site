11e6a4281c1882fc7a091fd38e3d99c7c4bc2fb9
T.Miura <mtaiga0213@icloud.com>
2025-07-15 12:02:52 +0900

    style: ヒーローページのレイアウトを調整

--- app/hero/page.tsx
+++ app/hero/page.tsx
@@ -12,36 +12,36 @@ export default function HeroPage() {
     <FullScreenPage>
       <div className="p-8 m-3 h-full">
         <div className="grid grid-cols-9 grid-rows-16 gap-4 h-full">
-        {/* 🔳 写真スライドショー - 左上 5x4 */}
-        <div className="col-span-4 row-span-8">
+        {/* 🔳 写真スライドショー - 左上 */}
+        <div className="col-span-4 row-span-10 col-start-1 row-start-1">
           <BorderedContainer>
             <SwiperGallery images={profileImages} />
           </BorderedContainer>
         </div>
 
-        {/* 🔸 キャッチコピー - 右上 4x2 */}
-        <div className="col-span-5 row-span-2">
+        {/* 🔸 キャッチコピー - 右上 */}
+        <div className="col-span-5 row-span-3 col-start-5 row-start-1">
           <BorderedContainer className="h-full">
             <CatchPhrase />
           </BorderedContainer>
         </div>
 
-        {/* 🔹 自己紹介 - 右中 4x3 */}
-        <div className="col-span-5 row-span-8">
+        {/* 🔹 自己紹介 - 右中 */}
+        <div className="col-span-5 row-span-10 col-start-5 row-start-4">
           <BorderedContainer>
             <SelfIntroduction />
           </BorderedContainer>
         </div>
 
-        {/* ▫️ 技術スタック - 左下 5x1 */}
-        <div className="col-span-4 row-span-3">
+        {/* ▫️ 技術スタック - 左下 */}
+        <div className="col-span-4 row-span-3 col-start-1 row-start-11">
           <BorderedContainer>
             <TechStack />
           </BorderedContainer>
         </div>
 
-        {/* 🔲 GitHubコントリビュートグラフ - 下部 9x2 */}
-        <div className="col-span-9 row-span-1">
+        {/* 🔲 GitHubコントリビュートグラフ - 下部 */}
+        <div className="col-span-9 row-span-2 col-start-1 row-start-14">
           <BorderedContainer>
             <GitHubContribution />
           </BorderedContainer>
