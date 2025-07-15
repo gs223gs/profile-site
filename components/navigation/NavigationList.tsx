bbe2317bdaea76187f2b3dc2a23720a374de223b
T.Miura <mtaiga0213@icloud.com>
2025-07-15 19:11:42 +0900

    feat: サイドバーの状態に応じてNavigationListのスタイルを変更

--- components/navigation/NavigationList.tsx
+++ components/navigation/NavigationList.tsx
@@ -1,19 +1,23 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
+import { useAtom } from 'jotai'
 import { routes } from '@/types/navigation'
 import NavigationItem from './NavigationItem'
+import { sidebarOpenAtom } from '@/atoms/sidebarAtoms'
 
 const NavigationList = () => {
   const pathname = usePathname()
+  const [isOpen] = useAtom(sidebarOpenAtom)
 
   return (
-    <div className="flex-1 p-4 space-y-2">
+    <div className={`flex-1 ${isOpen ? 'p-4' : 'p-2'} space-y-2`}>
       {routes.map((route) => (
         <NavigationItem
           key={route.href}
           route={route}
           isActive={pathname === route.href}
+          isCollapsed={!isOpen}
         />
       ))}
     </div>
