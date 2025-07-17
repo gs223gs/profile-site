5a1022bbdbba951798be6a59d27e958eee8784cf
T.Miura <mtaiga0213@icloud.com>
2025-07-17 22:56:46 +0900

    refactor: Linkの削除

--- components/hero/GitHubContribution.tsx
+++ components/hero/GitHubContribution.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import ContributionBar from "./ContributionBar";
 import { getMonthlyContributions } from "@/app/actions/githubApiFetch";
 
@@ -9,15 +8,8 @@ export const GitHubContribution = async () => {
   });
 
   return (
-    <Link
-      href="https://github.com/tm"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="block h-full hover:border-gray-300 hover:shadow-md transition-all cursor-pointer"
-    >
       <div className="flex items-center justify-center h-full">
         <ContributionBar monthly={monthly} />
       </div>
-    </Link>
   );
 };
