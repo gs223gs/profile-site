a0eff863f307a0bdca29c92729a488385917e225
T.Miura <mtaiga0213@icloud.com>
2025-07-17 20:04:24 +0900

    refactor: 使用していない jotaiを削除

--- components/layout/MainContent.tsx
+++ components/layout/MainContent.tsx
@@ -1,14 +1,9 @@
-'use client'
-
-import { useAtom } from 'jotai'
-import { sidebarOpenAtom } from '@/atoms/sidebarAtoms'
 
 interface MainContentProps {
   children: React.ReactNode
 }
 
 export function MainContent({ children }: MainContentProps) {
-  const [isOpen] = useAtom(sidebarOpenAtom)
 
   return (
     // <main className={isOpen ? 'ml-64' : 'ml-16'}>
