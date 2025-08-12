237cbf7c6fbda9b5c36e077d2051022008356ab5
T.Miura <mtaiga0213@icloud.com>
2025-08-12 23:18:26 +0900

    feat: キャリアを追加　LT 登壇

--- types/career.ts
+++ types/career.ts
@@ -10,12 +10,22 @@ export interface Career {
   readonly title: string
   /** 補足詳細（省略可） */
   readonly detail?: string
+  /** リンク */
+  readonly link?: string
+
   /** 1 件につき 1 〜 n 個のタグ */
   readonly tags: readonly Tag[]
 }
 
 /** キャリア一覧 */
 export const careers: readonly Career[] = [
+  {
+    date: '2025-08-26',
+    title: 'LT登壇',
+    detail: '個人開発/環境依存LT会【BEELT #11】',
+    // link: 'https://beenos.connpass.com/event/364554/',
+    tags: ['side'],
+  },
   {
     date: '2025-06 – Now',
     title: 'Next.js App Router 学習',
