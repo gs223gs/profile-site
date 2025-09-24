e4a0d0d261ec475abe9a8f1d7ec8c59169e92bcc
T.Miura <mtaiga0213@icloud.com>
2025-09-24 15:51:31 +0900

    自己紹介コンポーネントの内容を更新

--- components/hero/SelfIntroduction.tsx
+++ components/hero/SelfIntroduction.tsx
@@ -3,15 +3,11 @@ export const SelfIntroduction = () => {
     <div className="h-full flex flex-col justify-center p-4">
       <h2 className="text-2xl font-bold text-gray-900 mb-4">自己紹介</h2>
       <div className="space-y-3 text-gray-700">
+        <p>はじめまして、身長188cm {`=>`} T.Miuraです。</p>
+        <p>現在はJavaエンジニアを生業としております。</p>
         <p>
-          はじめまして、身長188cm => T.Miuraです。
-        </p>
-        <p>
-          現在はJavaエンジニアを生業としております。
-        </p>
-        <p>
-          個人ではReactやNext.jsを学習中です。
-		  ~~ プロダクトを作成中 ~~
+          個人ではReactやNext.jsを学習中です。 <br />
+          ~~ プロダクトを作成中 ~~
         </p>
         <p>
           私は「志は身長より高く」をモットーに
@@ -19,10 +15,11 @@ export const SelfIntroduction = () => {
           成長意欲や取り組む姿勢、それと身長は誰にも負けないと自負しております。
         </p>
         <p>
-		　将来的には Next.jsを軸にモダンフルスタックで活躍していきたいと考えています。
-		  また、行動力、継続力、主体性を活かしてお客様の真の課題を解決する．そんなエンジニアになりたいです。
+          将来的には
+          Next.jsを軸にモダンフルスタックで活躍していきたいと考えています。
+          また、行動力、継続力、主体性を活かしてお客様の真の課題を解決するエンジニアを目指しています．
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
